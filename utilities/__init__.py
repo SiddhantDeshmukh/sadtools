6aeaf0fbb3cbf83abf68908f502dcdcba64fbf45
Siddhant Deshmukh <sd486@exeter.ac.uk>
2020-11-02 09:34:18 +0100

    initial commit

---
+++ utilities/__init__.py
@@ -0,0 +1,7 @@
+from .chemistryUtilities import *
+from .dataframeUtilities import *
+from .dictUtilities import *
+from .listUtilities import *
+from .numerics import *
+from .sifit_utilities import *
+from .spectralUtilities import *
