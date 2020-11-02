4f727c3aba54874d1700fd321193e0394e3f0a6d
Siddhant Deshmukh <sd486@exeter.ac.uk>
2020-11-02 09:37:00 +0100

    astro tools

--- utilities/__init__.py
+++ utilities/__init__.py
@@ -3,5 +3,3 @@ from .dataframeUtilities import *
 from .dictUtilities import *
 from .listUtilities import *
 from .numerics import *
-from .sifit_utilities import *
-from .spectralUtilities import *
